7436a8cc4a7039978ecd4379f1056e211bd88c62
Donald Derek <donaldderek.haddad@gmail.com>
2014-02-07 17:14:59 +0200

    twit

--- app.js
+++ app.js
@@ -39,16 +39,29 @@ app.get('/', function (req, res) {
   res.render('index', { title: 'PiCam' });
 });
 
+//Twitter API
+var Twitter = require('node-twitter');
+
+var twitterRestClient = new Twitter.RestClient(
+    'kt5owDkV89MmMnnWYUegA',
+    'UAYCYuHiQx6fKrNQ6ajgzsU6MOPdJa8HckKWRGEaE',
+    '54840439-Nh8q4bNADyItt6wpbq6JnnBmS2Sb5D1ngh6Mf1hyN',
+    'a6EBsjUjWpfWjZS3wxCEjgIBNGQEVJMgPYgOBZWhAyMhc'
+);
+
 //Socket Events
 io.sockets.on('connection', function (socket) {
   socket.on('shoot', function (data) {
     var timestamp = Number(new Date()); 	
-  	console.log(data)
   	child = exec("raspistill -o "+image_dir+timestamp+".jpg -w 640 -h 480", function (error, stdout, stderr) {
   		socket.emit('preview', { name: timestamp+'.jpg' });
+      twitterRestClient.statusesUpdateWithMedia({
+            'status': 'Posting a tweet w/ attached media from RaspiCam @conversionpoint',
+            'media[]': image_dir+timestamp+".jpg"
+        },function(error, result) {});
+      });
   	});
   });
-});
 
 server.listen(app.get('port'), function(){
   console.log('Express and Socket.io server listening on port ' + app.get('port'));
