7f8675e14f4e8d1f29953a60e9e2bdccbe668989
Donald Derek <donaldderek.haddad@gmail.com>
2014-02-07 16:20:46 +0200

    button

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,14 @@
 (function () {
 	var socket = io.connect('http://' + window.location.host);
 	var preview_path = "images/"
-	socket.emit('shoot', { tweet: 'Lorem Ipssum Dolor' });
-	socket.on('preview', function (data) {
-		console.log(data.name);
-		var preview = document.getElementsByClassName("preview")[0].src=preview_path+data.name;
-	});
+	Zepto(function($){
+		$(".shoot").on('click',function(){
+			socket.emit('shoot', { tweet: 'Lorem Ipssum Dolor' });	
+		});
+		
+		socket.on('preview', function (data) {
+			console.log(data.name);
+			var preview = document.getElementsByClassName("preview")[0].src=preview_path+data.name;
+		});
+	})
 })();
